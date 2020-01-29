b8f939141906aa8130233fcb3f8f97d125c3952e
Owen Abbott <ocabbott@gmail.com>
2020-01-29 17:44:47 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -39,10 +39,10 @@ def total_even_pairs(src)
     for i in src do
       if i[0] % 2 == 0 and i[1] % 2 == 0 
         total += i[0] + i[1]
-      elsif i[0] % 2 == 0 and i[1] % 2 != 0
-        total += i[0]
-      elsif i[0] % 2 != 0 and i[1] % 2 == 0 
-        total += i[1]
+      #elsif i[0] % 2 == 0 and i[1] % 2 != 0
+       # total += i[0]
+      #elsif i[0] % 2 != 0 and i[1] % 2 == 0 
+       # total += i[1]
     end
   end
   return total
